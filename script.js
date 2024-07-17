7793ce2425173f8c63c0a81be24e4ae32de6ea04
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-18 00:54:59 +0500

    chap 12 done

---
+++ script.js
@@ -0,0 +1,76 @@
+// chap no 12 if else statment
+
+const badal=false;
+if (badal){
+    alert("raining in peshawar");
+}
+if (!badal){
+    alert("no chance of rain");
+}
+
+true
+const badal=true;
+if (badal){
+    alert("raining in peshawar");
+}
+if (!badal){
+    alert("no chance of rain");
+}
+
+const badal=true;
+if (badal){
+    alert("the rain is start");
+}
+else{
+    alert("no chance");
+}
+
+false
+
+const badal=false;
+if (badal){
+    alert("the rain is start");
+}
+else{
+    alert("no chance");
+}
+
+const bhook=true;
+const paisa=true;
+
+if ( bhook && paisa){
+    alert("get the burger");
+
+}
+else{
+    alert("so sad");
+}
+
+const bhook=false;
+const paisa=false;
+
+if ( bhook && paisa){
+    alert("get the burger");
+
+}
+else{
+    alert("so sad");
+}
+
+
+nagating value 
+const badal=false;
+console.log(!badal);
+  
+
+const userName=prompt("please apna name bati");
+const userInput=+prompt("please 1 number add");
+
+const reminder=userInput % 2 ;
+if (reminder ===0){
+    alert(userName+ "ap nai even number enter kia ");
+}
+else{
+    alert("bhi nahi app nai odd number enter kia ");
+}
+
